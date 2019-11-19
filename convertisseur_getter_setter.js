c071707297dfcdff51fc4cd251436986a2b01ebe
vcoquinot <vcoquinot@gmail.com>
2019-11-19 16:25:40 +0100

    Create convertisseur_getter_setter.js

---
+++ convertisseur_getter_setter.js
@@ -0,0 +1,15 @@
+jQuery(function($) {
+    //utilisation d'une méthode (ici val())
+    //comme getter puis comme setter
+
+    $("#euro").on("input", function() {
+        //console.log('"on change');
+        //val utilisé ocmme setter puis comme getter
+        if(isNaN($(this).val())){
+            console.log('Entrez un nombre');
+            $(this).val("");//remise à 0
+        } else {
+            $("#chf").val($(this).val() * 1.09);
+        }  
+    });
+ });
